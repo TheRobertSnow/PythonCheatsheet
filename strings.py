3f8f30750b1761d3feac54a43b06efcb1b56c8dd
Arnar Daði Steinþórsson <arnardadi99@hotmail.com>
2019-09-30 17:53:30 +0000

    byrjun á strings

---
+++ strings.py
@@ -0,0 +1,21 @@
+# cheat sheet um strengjavinnslu, output er það sem kæmi ef þu myndir gera print(myString)
+
+# ATH strengurinn er alltaf að breytast þannig ef þið prufið að gera print í þessu skjali mun það ekki koma rétt út
+myString = "This is a string"
+
+"""
+Delete the last character
+output - "This is a strin"
+"""
+myString = myString[:-1]
+"""
+Only get the last character
+output -  "g"
+"""
+myString = myString[-1]
+"""
+cut the first 3 characters and the last 2 characters.
+output - "s is a stri"
+"""
+myString = myString[3:-2]
+
